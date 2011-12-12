8cb1f7880ac37591756cc2be593556f63221ff22
Dustin Smith <dustinsmith1024@hotmail.com>
2011-12-11 22:49:32 -0600

    got team additions working

--- teams/urls.py
+++ teams/urls.py
@@ -5,13 +5,14 @@ urlpatterns = patterns('teams.views',
     #url(r'^$', 'index'),
     #url(r'^(?P<team_id>\d+)/$', 'details'),
     url(r'^mine/$', 'mine'),
+    url(r'^new/$', 'new'),
+    url(r'^(?P<team_id>\d+)/update/$', 'update'),
     url(r'^(?P<team_id>\d+)/captain/$', 'captain'),
-    url(r'^(?P<team_id>\d+)/roster/$', 'roster'),
     url(r'^$',
         ListView.as_view(queryset=Team.objects.order_by('name'),),
        ),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(model=Team,), name='team_details'
-       ),
+    url(r'^(?P<team_id>\d+)/$', 'view', name='view'),
+    #    DetailView.as_view(model=Team,), name='team_details'
+    #   ),
 )
 
--- teams/views.py
+++ teams/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.forms.models import inlineformset_factory
 from django.template import Context, loader
 from django.template import RequestContext
 from teams.models import Team, Player, TeamForm, PlayerForm
@@ -11,7 +12,7 @@ from django.views.decorators.csrf import csrf_protect
 
 @login_required
 @csrf_protect
-def mine(request):
+def new(request):
     # If coach
     c = {}
     c.update(csrf(request))
@@ -19,46 +20,16 @@ def mine(request):
         form = TeamForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
-            #
-            print 'team form save!'
-            form.save()
-            return HttpResponseRedirect('/') # Redirect after POST
+            team = form.save()
+            messages.add_message(request, messages.INFO, 'Team info created!')
+            return HttpResponseRedirect(reverse('view', args=(team.id,)))
     else:
-        #c = {}
-        #c.update(csrf(request))
         form = TeamForm() # An unbound form
 
-    #return render_to_response('teams/team_form.html', {
-    #    'form': form,
-    #     'c'
-    #})
-    return render_to_response("teams/team_form.html", {'form': form, 'c':c},
+    return render_to_response("teams/team_form.html", {'action': 'new', 'form': form, 'c':c},
                                context_instance=RequestContext(request))
-    # else
-    """
-    team = request.user.get_profile().team
-    template = loader.get_template('teams/team_detail.html')
-    c = Context({
-        'team': team,
-    })
-    return HttpResponse(template.render(c))
-    """
-"""
-def index(request):
-    teams = Team.objects.all()
-    #display = ', '.join([team.name for team in teams])
-    template = loader.get_template('teams/index.html')
-    c = Context({
-        'teams': teams,
-    })
-    return HttpResponse(template.render(c))
-    #return HttpResponse("Hello, world. You're at the poll index.")
 
-def details(request, team_id):
-    print 'details'
-    team = get_object_or_404(Team, pk=team_id)
-    return render_to_response('teams/details.html', {'team':team}, context_instance=RequestContext(request))
-"""
+
 def captain(request, team_id):
     team = get_object_or_404(Team, pk=team_id)
     try:
@@ -70,18 +41,42 @@ def captain(request, team_id):
         }, context_instance=RequestContext(request))
     else:
         messages.add_message(request, messages.INFO, 'Hello world.')
-        #chosen_captain.captain = True
-        #chosen.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button
         return HttpResponseRedirect(reverse('team_details', args=(team.id,)))
 
-def roster(request, team_id):
-    return HttpResponse("This team: {0}".format(team_id))
+@login_required
+@csrf_protect
+def update(request, team_id):
+    team = get_object_or_404(Team, pk=team_id)
+    c = {}
+    c.update(csrf(request))
+    if request.method == 'POST': # If the form has been submitted...
+        form = TeamForm(request.POST, instance=team) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            form.save()
+            messages.add_message(request, messages.INFO, 'Team info updated!')
+            return HttpResponseRedirect(reverse('view', args=(team.id,)))
+    else:
+        form = TeamForm(instance=team) # An unbound form
 
+    # pass action with the id, probably need a cleaner way of doing this
+    return render_to_response("teams/team_form.html", {'action': str(team.id) + '/update', 'form': form, 'c':c},
+                               context_instance=RequestContext(request))
 
+@login_required
+@csrf_protect
+def mine(request):
+    # If coach
+    team = Player.objects.get(user=request.user).team
+    return render_to_response('teams/team_detail.html', {'team':team}, context_instance=RequestContext(request))
 
-
+def view(request, team_id):
+    team = get_object_or_404(Team, pk=team_id)
+    #RosterForm = inlineformset_factory(Team, Player)
+    #formset = RosterForm(instance=team)
+    #print formset
+    return render_to_response('teams/team_detail.html', {'team':team}, context_instance=RequestContext(request))
 
 
