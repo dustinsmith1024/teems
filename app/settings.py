b790151a3e5a721537de2ecb1d2e65fb7c9d4e79
Dustin Smith <dustinsmith1024@hotmail.com>
2012-01-30 22:44:54 -0600

    designing roster

--- app/settings.py
+++ app/settings.py
@@ -75,6 +75,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, "static/css"),
     os.path.join(PROJECT_DIR, "static/js"),
+    os.path.join(PROJECT_DIR, "static/less"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
