9c0636be1039e219a952dc6e4e1a7ce25bc07647
Dustin Smith <dustinsmith1024@hotmail.com>
2012-02-04 00:51:02 -0600

    new roster styles

---
+++ app/compress.sh
@@ -0,0 +1,3 @@
+../../../../../node_modules/less/bin/lessc css/style.less -x > min_styles.css 
+../../../../../node_modules/less/bin/lessc less/bootstrap.less -x >> min_styles.css 
+../../../../../node_modules/less/bin/lessc less/responsive.less -x >> min_styles.css 
